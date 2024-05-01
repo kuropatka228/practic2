b654e4e59b857c8a490c893d6b83a27f3830d505
kuropatka228 <egorshumilo25@gmail.com>
2024-05-01 16:44:58 +0900

    main

---
+++ zxc.js
@@ -0,0 +1,11 @@
+const image = document.getElementById("responsive-image");
+
+function checkScreenSize() {
+  if (window.innerWidth <= 768) { 
+    image.src = "/img/people2_mini.png";
+  } else {
+    image.src = "/img/people2.png";
+  }
+}
+window.addEventListener("load", checkScreenSize);
+window.addEventListener("resize", checkScreenSize);
